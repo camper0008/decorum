179532289e1c0455e68f1be187d4dd44054c8be2
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-09-20 23:12:00 +0200

    notes and session handler

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,7 +5,8 @@ mod permission_utils;
 use std::sync::Arc;
 
 use db::{database::DatabaseParam, sqlite::SqliteDb};
-use salvo::prelude::*;
+use eyre::Context;
+use salvo::{prelude::*, session::CookieStore};
 use tokio::sync::RwLock;
 
 fn openapi_route(router: Router) -> Router {
@@ -15,15 +16,27 @@ fn openapi_route(router: Router) -> Router {
         .push(SwaggerUi::new("/api-doc/openapi.json").into_router("/swagger-ui"))
 }
 
+// TODO: rate limiting
+// TODO: everything else api
+// TODO: db impl
+
 #[tokio::main]
-async fn main() {
+async fn main() -> eyre::Result<()> {
     tracing_subscriber::fmt().init();
+
+    let secret = std::env::var("SESSION_HANDLER_TOKEN")
+        .with_context(|| "env variable `SESSION_HANDLER_TOKEN` should be set")?;
+
+    let session_handler = SessionHandler::builder(CookieStore::new(), secret.as_bytes())
+        .build()
+        .unwrap();
 
     let database = Arc::new(RwLock::new(SqliteDb {}));
 
     let router = Router::new();
     let router = router.push(Router::with_path("/health").get(api::health::route));
     let router = router
+        .hoop(session_handler)
         .hoop(affix::inject::<DatabaseParam>(database))
         .push(Router::with_path("/posts/create_post").post(api::posts::create_post_route));
 
@@ -31,4 +44,5 @@ async fn main() {
 
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
     Server::new(acceptor).serve(router).await;
+    Ok(())
 }
